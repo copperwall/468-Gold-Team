5685919da1d24d3e9857f441dfe6c3f2c8d49e08
Chris Opperwall <chriso@ifixit.com>
2016-04-04 19:04:49 -0700

    Mostly skellingtons, doot doot. 2 spoopy

---
+++ lab1/JSON2CSV.c
@@ -0,0 +1,28 @@
+#include "JSON2CSV.h"
+
+void record_add(Record *record, char *key, char *value) {
+
+}
+
+char *record_get(Record *record, char *key) {
+   return "lol";
+}
+
+Table *add_table(char *table_name) {
+   // Need to make a list of table with a size,
+   // This will initialize the next size and return a pointer to it.
+   Table *lol;
+   return lol;
+}
+
+void table_add_key(Table *table, char *key) {
+
+}
+
+void table_add_record(Table *table, Record *record) {
+
+}
+
+int table_lookup_index_for_key(Table *table, char *key) {
+   return 0; // lol
+}

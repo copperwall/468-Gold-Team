f9abaef3fe08ec64641a842680cdcdc298ae4455
Chris Opperwall <chriso@ifixit.com>
2016-04-06 15:33:42 -0700

    Add first draft of readObjects
    Still need to test this function, and flush out parse so that parse calls this
    function when |root| is an object.
    
    Also need to consider how to include primary keys with records.

--- lab1/JSON2CSV.c
+++ lab1/JSON2CSV.c
@@ -5,6 +5,7 @@
 
 Table tables[MAX_TABLES];
 int numTables;
+int firstObject;
 
 /**
  * Add a value to a record.
@@ -23,6 +24,15 @@ int numTables;
 void record_add(Record *record, char *key, char *value) {
 }
 */
+
+/**
+ * Copies the given primary key and returns a new key with an extra depth with
+ * value |key|.
+ */
+void add_to_key(PrimaryKey *pk, PrimaryKey *newPk, int key) {
+   memcpy(newPk, pk, sizeof(PrimaryKey));
+   newPk->key[newPk->numKeys++] = key;
+}
 
 void record_add(Record *r, int idx, char *value) {
    r->value[idx] = value;
@@ -79,7 +89,7 @@ void table_add_key(Table *table, char *key) {
  *       done after we've done all the manipulations to it.
  */
 void table_add_record(Table *table, Record *record) {
-   memcpy(&table->records[table->numRecords++], record, sizeof(record));
+   memcpy(&table->records[table->numRecords++], record, sizeof(Record));
 }
 
 /**
@@ -88,14 +98,18 @@ void table_add_record(Table *table, Record *record) {
  */
 int table_lookup_index_for_key(Table *table, char *key) {
    int i = 0;
-   for (i; i < table->numAttributes; i++) {
+   for (; i < table->numAttributes; i++) {
       if (strcmp(key, table->colNames[i]) == 0) {
          return i;
       }
    }
-   return 0; // lol
+
+   // Not found.
+   return -1;
 }
 
+// TODO: Either include primary keys when creating record or put primary as
+// part of table and prepend primary key values before other columns.
 void serialize(){
    FILE *fp;
    char *fileName;
@@ -134,6 +148,84 @@ void serialize(){
       free(fileName);
       i++;
    }
+
+}
+
+// TODO:
+// Consider if pk needs to be a pointer. I'm not sure it does.
+void readObjects(json_t *root, PrimaryKey *pk, Table *table) {
+   int json_type, outer = 0;
+   size_t index, record_index;
+   const char *key;
+   char value_str[MAX_TABLE_NAME];
+   json_t *value, *array_element;
+   Table *nextTable;
+   PrimaryKey newPk;
+   Record record;
+
+   if (pk->numKeys == 0) {
+      // This means that this is the top level of the JSON object.
+      // This is pseudo code, consider adding an outer flag
+      // Need to add the current id attribute to the primary key
+      add_to_key(pk, pk, json_integer_value(json_object_get(root, "id")));
+   }
+
+   json_object_foreach(root, key, value) {
+      // If is atomic
+      // atmoic is string, int, boolean
+      json_type = json_typeof(value);
+      switch (json_type) {
+         case JSON_STRING:
+            strcpy(value_str, json_string_value(value));
+            break;
+         case JSON_INTEGER:
+            sprintf(value_str, "%d", (int)json_integer_value(value));
+            break;
+         case JSON_REAL:
+            sprintf(value_str, "%0.2f", json_real_value(value));
+            break;
+         case JSON_TRUE:
+            strcpy(value_str, "true");
+            break;
+         case JSON_FALSE:
+            strcpy(value_str, "false");
+            break;
+         case JSON_NULL:
+            strcpy(value_str, "null");
+            break;
+         case JSON_OBJECT:
+            // Need to add table and initialize
+            nextTable = add_table(strncat(table->tableName, key, MAX_TABLE_NAME));
+            parse(value, pk, nextTable);
+            break;
+         case JSON_ARRAY:
+            nextTable = add_table(strncat(table->tableName, key, MAX_TABLE_NAME));
+            json_array_foreach(value, index, array_element) {
+               add_to_key(pk, &newPk, index);
+               parse(array_element, &newPk, nextTable);
+            }
+            break;
+      }
+
+      if (json_type != JSON_OBJECT && json_type != JSON_ARRAY) {
+         // TODO: Something needs to be done to add each primary key before the
+         // first columns in each table.
+         if (firstObject) {
+            // Add key to table if this is the first object in the collection.
+            table_add_key(table, (char *)key);
+         }
+         // Add attribute key and value to record
+         record_index = table_lookup_index_for_key(table, (char *)key);
+         record_add(&record, record_index, value_str);
+      }
+   }
+
+   // Add this point all fields have been iterated over, and the record should
+   // be complete. Add record to the table.
+   table_add_record(table, &record);
+}
+
+void parse(json_t *root, PrimaryKey* pk, Table *table) {
 
 }
 

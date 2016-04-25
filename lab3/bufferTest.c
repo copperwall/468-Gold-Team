f196a000de3faa0d2400aded931d50759a630dd4
Chris Opperwall <chriso@ifixit.com>
2016-04-25 00:16:44 -0700

    Harness: Add tinyfs filename support
    This adds a file argument to most of the harness to commands. This argument
    specifies a tinyfs file to open and operate on.

--- lab3/bufferTest.c
+++ lab3/bufferTest.c
@@ -4,25 +4,28 @@
 #include "bufman.h"
 #define MAX_LINE_SIZE 20
 
-// Take a fd,disk_address string and return the two values
+// Take a filename and disk address string and return the two values
 DiskAddress *handle_args(char *args) {
-   char *fd, *pageid;
+   char *filename, *pageid;
    DiskAddress *address = malloc(sizeof(DiskAddress));
 
-   fd = strsep(&args, ",");
-   pageid = strsep(&args, ",");
+   filename = strsep(&args, " ");
+   pageid = strsep(&args, " ");
 
-   address->FD = atoi(fd);
+   address->FD = tfs_openFile(filename);
    address->pageId = atoi(pageid);
 
    return address;
 }
 
+// Close the file that was worked on.
+void teardown(DiskAddress *address) {
+   tfs_closeFile(address->FD);
+   free(address);
+}
+
 // Needs to read from file and call commands for the following inputs
 // While reading from file is not null
-// Strtok on whitespace to split the string
-// Take the first of the strtok to be the command
-// Based on the command handle the options
 int main(int argc, char *argv[]) {
    char *line = NULL;
    char *command;
@@ -49,7 +52,7 @@ int main(int argc, char *argv[]) {
       }
 
       if (!strcmp(command, "start")) {
-         // Expect a filename and a number of blocks
+         // Expect a diskName and a number of blocks
          char *diskName = strsep(&line, " ");
          char *numBlocks = strsep(&line, " ");
          int nBlocks;
@@ -57,49 +60,55 @@ int main(int argc, char *argv[]) {
          nBlocks = atoi(numBlocks);
          commence(diskName, buf, nBlocks);
       } else if (!strcmp(command, "end")) {
-         // No args
          squash(buf);
          return 0;
       } else if (!strcmp(command, "read")) {
-         // Takes fd,pageid
          address = handle_args(line);
-         printf("Reading page with fd %d and pageid %d\n", address->FD, address->pageId);
          readPage(buf, *address);
+         teardown(address);
       } else if (!strcmp(command, "write")) {
-         // Takes fd,pageid
          address = handle_args(line);
          writePage(buf, *address);
+         teardown(address);
       } else if (!strcmp(command, "flush")) {
-         // Takes fd,pageid
          address = handle_args(line);
          flushPage(buf, *address);
+         teardown(address);
       } else if (!strcmp(command, "pin")) {
-         // Takes fd,pageid
          address = handle_args(line);
          pinPage(buf, *address);
+         teardown(address);
       } else if (!strcmp(command, "unpin")) {
-         // Takes fd,pageid
          address = handle_args(line);
          unPinPage(buf, *address);
+         teardown(address);
       } else if (!strcmp(command, "new")) {
-         // Takes num pages
-         char *numPages = strsep(&line, " ");
-         int nPages = atoi(numPages);
+         // Takes file, begin page, end page
+         char *filename = strsep(&line, " ");
+         char *page1 = strsep(&line, " ");
+         char *page2 = strsep(&line, " ");
 
-         int i;
-         for (i = 0; i < nPages; i++) {
+         int i, beginPage, endPage, tfs_fd;
+
+         tfs_fd = tfs_openFile(filename);
+         beginPage = atoi(page1);
+         endPage = atoi(page2);
+         newAddress.FD = tfs_fd;
+
+         for (i = beginPage; i <= endPage; i++) {
+            newAddress.pageId = i;
             newPage(buf, tfs_fd, &newAddress);
          }
          for (i = 0; i < buf->numOccupied; i++) {
             printf("FD: %d pageid: %d\n", buf->pages[i].diskPage.FD, buf->pages[i].diskPage.pageId);
          }
+
+         tfs_closeFile(tfs_fd);
       } else if (!strcmp(command, "check")) {
          // Call checkpoint of Test API
          checkpoint(buf);
       } else {
          printf("Unknown command: %s\n", command);
-         printf("%d\n", strcmp(command, "end"));
-         /* exit(-1); */
       }
    }
 }

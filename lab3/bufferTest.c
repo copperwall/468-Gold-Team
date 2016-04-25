0e5f406a40f17e12f5a257abf703965c863fc592
Chris Opperwall <chriso@ifixit.com>
2016-04-25 13:49:10 -0700

    Harness: keep track of open fds

--- lab3/bufferTest.c
+++ lab3/bufferTest.c
@@ -4,24 +4,41 @@
 #include "bufman.h"
 #define MAX_LINE_SIZE 20
 
+static char **openFiles = NULL;
+static int openFileSize = 0;
+
+int checkOpenFiles(char *filename) {
+   int i;
+
+   for (i = 0; i < openFileSize; i++) {
+      if (!strcmp(openFiles[i], filename)) {
+         return i;
+      }
+   }
+
+   return -1;
+}
+
 // Take a filename and disk address string and return the two values
 DiskAddress *handle_args(char *args) {
    char *filename, *pageid;
    DiskAddress *address = malloc(sizeof(DiskAddress));
+   int fd = -1;
 
    filename = strsep(&args, " ");
    pageid = strsep(&args, " ");
 
-   address->FD = tfs_openFile(filename);
+   if ((fd = checkOpenFiles(filename)) >= 0) {
+      address->FD = fd;
+   } else {
+      address->FD = tfs_openFile(filename);
+      // Add new fd to list of open files
+      strcpy(openFiles[address->FD], filename);
+   }
+
    address->pageId = atoi(pageid);
 
    return address;
-}
-
-// Close the file that was worked on.
-void teardown(DiskAddress *address) {
-   tfs_closeFile(address->FD);
-   free(address);
 }
 
 // Needs to read from file and call commands for the following inputs
@@ -33,7 +50,7 @@ int main(int argc, char *argv[]) {
    DiskAddress *address, newAddress;
    Buffer *buf = calloc(1, sizeof(Buffer));
    FILE *input;
-   int tfs_fd = 0;
+   int read, tfs_fd = 0;
 
    if (argc != 2) {
       printf("Usage: ./harness <filename>\n");
@@ -42,7 +59,8 @@ int main(int argc, char *argv[]) {
 
    input = fopen(argv[1], "r");
 
-   while (getline(&line, &size, input)) {
+   while ((read = getline(&line, &size, input)) != -1) {
+
       // strsep the line based on
       printf("%s\n", line);
       command = strsep(&line, " ");
@@ -55,7 +73,14 @@ int main(int argc, char *argv[]) {
          // Expect a diskName and a number of blocks
          char *diskName = strsep(&line, " ");
          char *numBlocks = strsep(&line, " ");
-         int nBlocks;
+         int nBlocks, i;
+         openFiles = calloc(size, sizeof(char *));
+         openFileSize = size;
+
+         for (i = 0; i < size; i++) {
+            openFiles[i] = calloc(MAX_LINE_SIZE, sizeof(char));
+         }
+
 
          nBlocks = atoi(numBlocks);
          commence(diskName, buf, nBlocks);
@@ -65,28 +90,24 @@ int main(int argc, char *argv[]) {
       } else if (!strcmp(command, "read")) {
          address = handle_args(line);
          readPage(buf, *address);
-         teardown(address);
       } else if (!strcmp(command, "write")) {
          address = handle_args(line);
          writePage(buf, *address);
-         teardown(address);
       } else if (!strcmp(command, "flush")) {
          address = handle_args(line);
          flushPage(buf, *address);
-         teardown(address);
       } else if (!strcmp(command, "pin")) {
          address = handle_args(line);
          pinPage(buf, *address);
-         teardown(address);
       } else if (!strcmp(command, "unpin")) {
          address = handle_args(line);
          unPinPage(buf, *address);
-         teardown(address);
       } else if (!strcmp(command, "new")) {
          // Takes file, begin page, end page
          char *filename = strsep(&line, " ");
          char *page1 = strsep(&line, " ");
          char *page2 = strsep(&line, " ");
+         printf("New file, %s, %s, %s\n", filename, page1, page2);
 
          int i, beginPage, endPage, tfs_fd;
 
@@ -99,16 +120,13 @@ int main(int argc, char *argv[]) {
             newAddress.pageId = i;
             newPage(buf, tfs_fd, &newAddress);
          }
-         for (i = 0; i < buf->numOccupied; i++) {
-            printf("FD: %d pageid: %d\n", buf->pages[i].diskPage.FD, buf->pages[i].diskPage.pageId);
-         }
 
          tfs_closeFile(tfs_fd);
       } else if (!strcmp(command, "check")) {
          // Call checkpoint of Test API
          checkpoint(buf);
       } else {
-         printf("Unknown command: %s\n", command);
+         printf("Unknown command: %s size %d\n", command, size);
       }
    }
 }

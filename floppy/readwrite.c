14f5d1aa91cc47beff4547247e1b2227bd747d25
Chris Opperwall <copperwa@calpoly.edu>
2016-05-07 20:12:44 -0700

    Throw in initial implementations of read,write,put/getPage
    I wrote read and write as special cases of get/putPage because I thought it
    would be nice to encapsulate buffer access through only getPage and putPage.
    
    Someone during lecture also mentioned using a `char *` as an out parameter during
    class instead of returning a char * of the data read. I thought that was a
    little easier on read, write, getPage, and putPage because those function don't
    have to worry about allocating memory for the bytearray that they return and
    can let the caller handle it.

--- floppy/readwrite.c
+++ floppy/readwrite.c
@@ -1,6 +1,95 @@
+#include <string.h>
+#include <stdlib.h>
 #include "floppy.h"
 #include "bufman.h"
+#include "readwrite.h"
 
 /**
  * Begin readwrite functions
  */
+
+// Search volatile buffer and non-volatile buffer for diskAddress
+// TODO: If the page isn't in the buffer, should we attempt to read it from
+// disk? How do we know if that pageid "exists" yet? Check the number of pages
+// in the file? I feel like this problem in more in the scope of readPage()
+int getPage(Buffer *buf, DiskAddress page, char *out) {
+   int index;
+
+   index = getCacheIndex(buf, page);
+
+   if ((index = getBufferIndex(buf, page)) == ERROR) {
+      if ((index = getBufferIndex(buf, page)) == ERROR) {
+         return ERROR;
+      }
+
+      memcpy(out, buf->pages[index].block, BLOCKSIZE);
+      return SUCCESS;
+   }
+
+   memcpy(out, buf->cache[index].block, BLOCKSIZE);
+   return SUCCESS;
+}
+
+int putPage(Buffer *buf, DiskAddress page, char *data, int dataSize) {
+   int index;
+   if (dataSize < BLOCKSIZE) {
+      return ERROR;
+   }
+
+   if ((index = getBufferIndex(buf, page)) == ERROR) {
+      if ((index = getBufferIndex(buf, page)) == ERROR) {
+         // TODO: Make new page
+         // UNIMPLEMENTED
+         return ERROR;
+      }
+
+      memcpy(buf->pages[index].block, data, BLOCKSIZE);
+      writePage(buf, page);
+   } else {
+      memcpy(buf->cache[index].block, data, BLOCKSIZE);
+   }
+
+   return SUCCESS;
+}
+
+int read(Buffer *buf, DiskAddress page, int startOffset, int nBytes, char *out) {
+   char block[BLOCKSIZE];
+   int result = getPage(buf, page, block);
+
+   if (result == ERROR || startOffset >= BLOCKSIZE) {
+      return ERROR;
+   }
+
+   memcpy(out, block + startOffset, nBytes);
+   return SUCCESS;
+}
+
+int write(Buffer *buf, DiskAddress page, int startOffset, int nBytes, char *data, int dataSize) {
+   char block[BLOCKSIZE];
+   int result;
+
+   if (startOffset >= BLOCKSIZE) {
+      return ERROR;
+   }
+
+   result = getPage(buf, page, block);
+
+   if (result == ERROR) {
+      // TODO: NEW PAGE
+      // UNIMPLEMENTED
+      return ERROR;
+   }
+
+   if (startOffset + nBytes >= BLOCKSIZE) {
+      return ERROR;
+   }
+
+   if (dataSize < nBytes) {
+      // write |dataSize| bytes, pad the rest up to nBytes with 0
+      memcpy(block + startOffset, data, nBytes);
+   }
+
+   if (dataSize >= nBytes) {
+      memcpy(block + startOffset, data, nBytes);
+   }
+}
--- floppy/readwrite.h
+++ floppy/readwrite.h
@@ -1,11 +1,8 @@
 /* readewrite.h */
 
-char * getPage(Buffer * buf, DiskAddress page);
-
-// I think we can assume that the size of |data| will be the size of a page.
-int putPage(Buffer buf, DiskAddress page, char * data);
-char * read(Buffer * buf, DiskAddress page, int startOffset, int nBytes);
-// I think Dekhtyar said we could use another parameter for the size of data.
-int write(Buffer * buf, DiskAddress page, int startOffset, int nBytes, char * data);
-char * readVolatile(Buffer * buf, DiskAddress page, int startOffset, int nBytes);
-int writeVolatile(Buffer* buf, DiskAddress page, int startOffset, int nBytes, char * data);
+int getPage(Buffer *buf, DiskAddress page, char *out);
+int putPage(Buffer *buf, DiskAddress page, char *data, int dataSize);
+int read(Buffer *buf, DiskAddress page, int startOffset, int nBytes, char *out);
+int write(Buffer *buf, DiskAddress page, int startOffset, int nBytes, char *data, int dataSize);
+int readVolatile(Buffer *buf, DiskAddress page, int startOffset, int nBytes, char *out);
+int writeVolatile(Buffer *buf, DiskAddress page, int startOffset, int nBytes, char *data);

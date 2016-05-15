d2ae6de2163391919b686d20dac753e28c6584c9
Chris Opperwall <copperwa@calpoly.edu>
2016-05-15 12:34:38 -0700

    Add volatile readwrite functions

--- floppy/readwrite.c
+++ floppy/readwrite.c
@@ -28,8 +28,32 @@ int getPage(Buffer *buf, DiskAddress page, char *out) {
    return SUCCESS;
 }
 
+// Need to check volatile storage
+// persistent storage and check for isVolatile
+int getVolatilePage(Buffer *buf, DiskAddress page, char *out) {
+   int index;
+
+   if ((index = getCacheIndex(buf, page)) == ERROR) {
+      if ((index = getBufferIndex(buf, page)) == ERROR) {
+         return ERROR;
+      }
+
+      // Make sure this page is volatile.
+      if (!buf->pages[index].isVolatile) {
+         return ERROR;
+      }
+
+      memcpy(out, buf->pages[index].block, BLOCKSIZE);
+      return SUCCESS;
+   }
+
+   memcpy(out, buf->cache[index].block, BLOCKSIZE);
+   return SUCCESS;
+}
+
 int putPage(Buffer *buf, DiskAddress page, char *data, int dataSize) {
    int index;
+
    if (dataSize < BLOCKSIZE) {
       return ERROR;
    }
@@ -48,6 +72,36 @@ int putPage(Buffer *buf, DiskAddress page, char *data, int dataSize) {
    }
 
    return SUCCESS;
+}
+
+// Check volatile storage
+int putVolatilePage(Buffer *buf, DiskAddress page, char *data, int dataSize) {
+   int index;
+
+   if (dataSize < BLOCKSIZE) {
+      return ERROR;
+   }
+
+   if ((index = getCacheIndex(buf, page)) == ERROR) {
+      if ((index = getBufferIndex(buf, page)) == ERROR) {
+         // TODO: Make new page
+         // UNIMPLEMENTED
+         return ERROR;
+      }
+
+      // Make sure this page is volatile.
+      if (!buf->pages[index].isVolatile) {
+         return ERROR;
+      }
+
+      memcpy(buf->pages[index].block, data, BLOCKSIZE);
+      writePage(buf, page);
+   } else {
+      memcpy(buf->cache[index].block, data, BLOCKSIZE);
+   }
+
+   return SUCCESS;
+
 }
 
 int read(Buffer *buf, DiskAddress page, int startOffset, int nBytes, char *out) {
@@ -88,5 +142,50 @@ int write(Buffer *buf, DiskAddress page, int startOffset, int nBytes, char *data
    } else {
       memcpy(block + startOffset, data, nBytes);
    }
+
+   putPage(buf, page, block, BLOCKSIZE);
+   return SUCCESS;
+}
+
+int readVolatile(Buffer *buf, DiskAddress page, int startOffset, int nBytes, char *out) {
+   char block[BLOCKSIZE];
+   int result = getVolatilePage(buf, page, block);
+
+   if (result == ERROR || startOffset >= BLOCKSIZE) {
+      return ERROR;
+   }
+
+   memcpy(out, block + startOffset, nBytes);
+   return SUCCESS;
+}
+
+int writeVolatile(Buffer *buf, DiskAddress page, int startOffset, int nBytes, char *data, int dataSize) {
+   char block[BLOCKSIZE];
+   int result;
+
+   if (startOffset >= BLOCKSIZE) {
+      return ERROR;
+   }
+
+   result = getVolatilePage(buf, page, block);
+
+   if (result == ERROR) {
+      // TODO: NEW PAGE
+      // UNIMPLEMENTED
+      return ERROR;
+   }
+
+   if (startOffset + nBytes >= BLOCKSIZE) {
+      return ERROR;
+   }
+
+   if (dataSize < nBytes) {
+      // write |dataSize| bytes, pad the rest up to nBytes with 0
+      memcpy(block + startOffset, data, nBytes);
+   } else {
+      memcpy(block + startOffset, data, nBytes);
+   }
+
+   putPage(buf, page, block, BLOCKSIZE);
    return SUCCESS;
 }
--- floppy/readwrite.h
+++ floppy/readwrite.h
@@ -5,4 +5,4 @@ int putPage(Buffer *buf, DiskAddress page, char *data, int dataSize);
 int read(Buffer *buf, DiskAddress page, int startOffset, int nBytes, char *out);
 int write(Buffer *buf, DiskAddress page, int startOffset, int nBytes, char *data, int dataSize);
 int readVolatile(Buffer *buf, DiskAddress page, int startOffset, int nBytes, char *out);
-int writeVolatile(Buffer *buf, DiskAddress page, int startOffset, int nBytes, char *data);
+int writeVolatile(Buffer *buf, DiskAddress page, int startOffset, int nBytes, char *data, int dataSize);

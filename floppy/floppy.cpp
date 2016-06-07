da5a9e823e795baac5fad2d0eaf9e232d65925b6
nickmasta <nverbos@calpoly.edu>
2016-06-07 16:15:59 -0700

    Everything compiles and the parser is working

--- floppy/floppy.cpp
+++ floppy/floppy.cpp
@@ -12,7 +12,48 @@ extern "C" {
 #include "heap.h"
 }
 
-int main()
-{
-   std::cout << "Hello World!!!!!";
+
+void executeStatement(char *statement){
+   FLOPPYOutput *output = FLOPPYParser::parseFLOPPYString(statement);
+   if (output->isValid) { // Use isValid flag to detect is parser parsed correctly.
+      switch(output->statement->type()) {
+         case(StatementType::ErrorStatement) : 
+            std::cout << "Found Error Statement" << std::endl;
+            break;
+         case(StatementType::CreateTableStatement) :
+            std::cout << "Found Create Table Statement" << std::endl;
+            break;
+         case(StatementType::DropTableStatement) :
+            std::cout << "Found Drop Table Statement" << std::endl;
+            break;
+         case(StatementType::CreateIndexStatement) :
+            std::cout << "Found Create Index Statement" << std::endl;
+            break;
+         case(StatementType::DropIndexStatement) :
+            std::cout << "Found Drop Index Statement" << std::endl;
+            break;
+         case(StatementType::InsertStatement) :
+            std::cout << "Found Insert Statement" << std::endl;
+            break;
+         case(StatementType::DeleteStatement) :
+            std::cout << "Found Delete Statement" << std::endl;
+            break;
+         case(StatementType::UpdateStatement) :
+            std::cout << "Found Update Statement" << std::endl;
+            break;
+         case(StatementType::SelectStatement) :
+            std::cout << "Found Select Statement" << std::endl;
+            break;
+         default :
+            std::cout << "Unknown statement type" << std::endl;
+            break;
+      }
+   }
+   else {
+      std::cout << "Parse Failed" << std::endl;
+   }
+}
+
+int main(int argc, char *argv[]) {
+   executeStatement("DROP TABLE test;");
 }

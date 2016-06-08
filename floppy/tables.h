aaf7e33046c0ffb093463bc6cca6c727414e2138
Chris Opperwall <chriso@ifixit.com>
2016-06-08 10:06:06 -0700

    Add getTableDescription

--- floppy/tables.h
+++ floppy/tables.h
@@ -4,3 +4,4 @@
 int createPersistentTable(Buffer *buf, tableDescription table);
 int createVolatileTable(Buffer *buf, tableDescription table);
 int getFileDescriptorForTable(Buffer *buf, char *name);
+tableDescription *getTableDescription(Buffer *buf, char *tableName, tableDescription *out);
